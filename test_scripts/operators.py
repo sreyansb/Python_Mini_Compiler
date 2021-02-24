766b2e3a4dbe174845539cfc2f650dac2a9e7f26
Sahith Kurapati <sahith02@gmail.com>
2021-02-24 16:10:55 +0530

    added operators test script

---
+++ test_scripts/operators.py
@@ -0,0 +1,50 @@
+from __future__ import print_function
+
+a = 3
+b = 7
+c = [7, 8]
+d = 15
+
+print("+")
+print(a + b)
+
+print("-")
+print(a - b)
+
+print("*")
+print(a * b)
+
+print("/")
+print(a / b)
+
+print("//")
+print(a // b)
+
+print("%")
+print(b % a)
+
+print("and")
+print(a and b)
+print(a and b and d)
+
+print("or")
+print(a or b)
+print(a or b or d)
+
+print("<")
+print(a < b)
+
+print(">")
+print(a > b)
+
+print("==")
+print(a == b)
+
+print("<=")
+print(a <= b)
+
+print(">=")
+print(a >= b)
+
+print("list access operator")
+print(c[1])
