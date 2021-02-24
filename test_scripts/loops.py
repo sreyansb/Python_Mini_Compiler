2fd8797450691f7945f787c2439c3e0e7c35bbbf
Sahith Kurapati <sahith02@gmail.com>
2021-02-24 15:52:18 +0530

    added loops test cases

---
+++ test_scripts/loops.py
@@ -0,0 +1,42 @@
+f = 0
+
+while f < (10):
+	m = 1
+	f = f + 1
+
+# print("m=", m)
+
+while False:
+	pass
+	print("Executed else branch for False condition while loop")
+
+while True:
+	print("Inside while true loop")
+	break
+	print("Executed else branch for True condition while loop")
+
+
+
+for x in range(7):
+	pass
+	print("Executed else branch for no break for loop")
+
+for x in range(7):
+	print(x)
+	break
+	print("Executed else branch despite break in for loop")
+
+for i in [10, 20]:
+	print(i)
+	#print("ha")
+	for j in range(10, 20):
+		print(j)
+		#a = 5
+	#print(j)
+
+for j in range(5):
+	print(j)
+	while False:
+		print("Inside False")
+		for l in range(3, 10):
+			print(l)
