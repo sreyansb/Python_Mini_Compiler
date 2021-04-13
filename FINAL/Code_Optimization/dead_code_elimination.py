605364b81fb06efc93321cfe17a715c40269e2b2
Sreyans Bothra <sreyansrocks@gmail.com>
2021-04-13 22:43:29 +0530

    loop invariance

---
+++ FINAL/Code_Optimization/dead_code_elimination.py
@@ -0,0 +1,35 @@
+import csv
+import copy
+
+PATH_TO_CSV = r"./non_optimized/test1.tsv"
+PATH_TO_OUTPUT_1 = r"./optimized/test_output_1.tsv"
+
+file_input = open(PATH_TO_CSV)
+quads = list(csv.reader(file_input, delimiter='\t'))
+
+def dead_code_elimination(quads):
+    flag = True
+    remove = True
+    while(flag):
+        flag = False
+        for i in range(len(quads)):
+            remove = True
+            if(not (quads[i][4] == "-" or quads[i][1].lower() in ["label", "goto", "if false","if"])):
+                for j in range(len(quads)):
+                    if((quads[i][4] == quads[j][2] and quads[j][0] != "-1") or (quads[i][4] == quads[j][3] and quads[j][0] != "-1")):
+                        remove = False
+                if((remove == True) and (quads[i][0] != "-1")):
+                    quads[i][0] = "-1"
+                    flag = True
+    return quads
+
+quads_copy = copy.deepcopy(quads)
+quads_output_1 = dead_code_elimination(quads_copy[1:])
+quads_output_1.insert(0, quads[0])
+
+file_output = open(PATH_TO_OUTPUT_1, "w")
+csv_writer = csv.writer(file_output, delimiter='\t', lineterminator = "\n")
+csv_writer.writerows(quads_output_1)
+file_output.flush()
+file_output.close()
+file_input.close()
---
+++ FINAL/Code_Optimization/tests/invariance.py
@@ -0,0 +1,9 @@
+a="hello"
+d=6
+c=a
+while True:
+	e=a
+	for i in range(10):
+		b=d
+		while False:
+			f=5
